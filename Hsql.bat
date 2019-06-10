328f9907cf4268562467fed285d5397fa6424058
justaway <eastaveracruz@gmail.com>
2019-06-11 00:40:10 +0300

    Bag fix 0.1

--- Hsql.bat
+++ Hsql.bat
@@ -1,6 +1,7 @@
-rem set FILM_ROOT= d:\trainSpring2019\michael\Filmservice-master\
-d:
+set FILM_ROOT= d:\trainSpring2019\michael\Filmservice-master\
+rem set CATALINA_HOME= c:\dev\tomcat\apache-tomcat-9.0.20\
+
+e:
 cd %FILM_ROOT%
 
-rem mvn clean package && COPY /Y %FILM_ROOT%\target\filmservice.war %CATALINA_HOME%\webapps\
 mvn clean package -Dmaven.test.skip=true && COPY /Y %FILM_ROOT%\target\filmservice.war %CATALINA_HOME%\webapps\
